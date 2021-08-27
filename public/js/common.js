7727d542f63078a2f028a13c99a960463c5bf664
ian <justforian0915@gmail.com>
2021-08-28 03:27:20 +0800

    update

--- public/js/common.js
+++ public/js/common.js
@@ -28,5 +28,27 @@ $("#submitPostButton").click(() => {
 });
 
 function createPostHtml(postData) {
-  return postData.content;
+  console.log(postData);
+  var postedBy = postData.postedBy;
+  var displayNmae = `${postedBy.firstName} ${postedBy.lastName}`;
+  var datetamp = postedBy.createdAt;
+  return `<div class='post'>
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}' >
+                    </div>
+                    <div class='postContentContainer'>
+                        <div class='header'>
+                            <a href='/profile/${postedBy.username}'>${displayNmae}</a>
+                            <span class="username">@${postedBy.username}</span>
+                            <span class="date">${datetamp}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 }
